bc9ea64a675dd227c6264fe44599c075bbda560d
HyeJu Jung <jhyeju1@gmail.com>
2025-05-01 12:45:40 -0600

    Assignment Part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import './App.css';
 import { useSpring, animated } from '@react-spring/web';
-
-
-function Number({n}) {
-  const {number} = useSpring({
-    from: {number:0},
-    to: {number:n},
-    delay: 200,
-    config: {mass:1, tension:10, friction:10},
-  })
-  return <animated.div style={{  fontFamily: 'Gmarket Sans', display: 'inline'}}>
-    {number.to((n) => n.toFixed(0))}
-  </animated.div>;
-}
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [currentNumber, updateCurrentNumber] = useState(1);
+  const targetNumber = 147;
+  
+  // Animation to count up 1-147
+  const countUpAnimation = useSpring({
+    from: { number: 1 },
+    number: currentNumber,
+    config: {
+      mass: 5,
+      friction: 120,
+      tension: 150,
+    },
+  });
+
+  //generate after 600ms
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      updateCurrentNumber(targetNumber);
+    }, 600);
+    //only one time
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className='App'>
+    <div>
       <h1>
-        <Number n={147}/>만
+        <animated.span>
+          {countUpAnimation.number.to((n) => Math.round(n))}
+        </animated.span>
+        만
       </h1>
     </div>
   );
